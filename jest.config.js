6b099c5f4b16011a69ed5dda5f0b5254e3fdcbc4
Jordach Makaya H.M <4thinkai@gmail.com>
2024-09-22 02:04:35 +0100

    update .prettierignore and setup prettier

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,28 @@
 module.exports = {
   moduleFileExtensions: ["js", "json", "ts"],
-  rootDir: "src",
+  rootDir: "./",
+  modulePaths: ["<rootDir>"],
   testRegex: ".*\\.spec\\.ts$",
   transform: {
     "^.+\\.(t|j)s$": "ts-jest",
   },
-  collectCoverageFrom: ["**/*.(t|j)s"],
-  coverageDirectory: "../coverage",
+  collectCoverageFrom: [
+    "**/*.(t|j)s",
+    "!src/**/*.d.ts", // Exclude type definition files
+    "!src/**/*.spec.ts", // Exclude test files from coverage
+  ],
+  testResultsProcessor: "jest-junit", //jest-junit | jest-sonar-reporter
+  coverageDirectory: "./coverage", // Make sure this is correct
   testEnvironment: "node",
-  coverageReporters: ["text", "lcov"],
+  coverageReporters: ["json", "lcov", "text", "clover"],
+
   reporters: [
     "default",
     [
-      "jest-junit",
+      "jest-junit", // Alternative outputName: jest-sonar-reporter | jest-junit"",
       {
-        outputDirectory: "./coverage",
-        outputName: "junit.xml",
+        outputDirectory: "./coverage", // Ensure this matches coverageDirectory if needed
+        outputName: "junit.xml", // Alternative outputName: "junit.xml | test-report.xml",
       },
     ],
   ],
